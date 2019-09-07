16d2a90ba9b018630f3cbd02307decc799b05800
Stephanie Weber <sweber@thoughtworks.com>
2019-09-07 11:36:34 -0700

    Webs - update packages & remove deprecated ones

--- webpack.config.js
+++ webpack.config.js
@@ -14,24 +14,25 @@ module.exports = {
             { test: /.jsx?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
-                query: { presets: ['es2015', 'react'] }
+                query: { presets: ["@babel/preset-react",
+                        "@babel/preset-env"] }
             },
-            { test: /\.png$/, 
-              loader: "url-loader?limit=100000" 
+            { test: /\.png$/,
+              loader: "url-loader?limit=100000"
             },
-            { test: /\.jpg$/, 
-              loader: "file-loader" 
+            { test: /\.jpg$/,
+              loader: "file-loader"
             },
-            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
               loader: 'url?limit=10000&mimetype=application/font-woff'
             },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
               loader: 'url?limit=10000&mimetype=application/octet-stream'
             },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
               loader: 'file'
             },
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
               loader: 'url?limit=10000&mimetype=image/svg+xml'
             }
         ]
